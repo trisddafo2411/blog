f226ab43b05c5f4787ab69a8e8bb663ee60b13bc
trissdafo2411 <xaxixo333@gmail.com>
2021-05-30 18:01:27 +0700

    lmao

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
+
 app.get('/', (req, res) => {
-  var a = 1;
-  var b = 2;
-  var c = a + b;
   res.send('Hello World!')
 });
 
