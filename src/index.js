83923843bb46ea78b366295699e424a779115da2
trissdafo2411 <xaxixo333@gmail.com>
2021-06-05 17:44:56 +0700

    form default behavior

--- src/index.js
+++ src/index.js
@@ -23,11 +23,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/news', (req, res) => {
+  console.log(req.query.q);
   res.render('news');
 });
 
 app.get('/search', (req, res) => {
-  console.log(req.query.q);
   res.render('search');
 });
 
