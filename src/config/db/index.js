3c902b5f763ac6f04a2f8a29b44d16263582668a
trissdafo2411 <xaxixo333@gmail.com>
2021-06-08 17:54:10 +0700

    model

---
+++ src/config/db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+async function connect() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/f8_education_dev', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        });
+        console.log('Connect successful');
+    } catch (error) {
+        console.log('Connect failed');
+    }
+}
+
+module.exports = { connect };
--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+//Connect to DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,7 +31,7 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 // console.log(path.join(__dirname, 'resources/views'));
 
 //home,search,contact,v.v..
@@ -41,5 +45,5 @@ route(app);
 // });
 //127.0.0.1 - localhost
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
