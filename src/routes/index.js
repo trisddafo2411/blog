de0d3454f83842fbf5cafdbbdff97927a37c1631
trissdafo2411 <xaxixo333@gmail.com>
2021-06-10 18:30:18 +0700

    courses detail

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
+const coursesRouter = require('./courses');
 function route(app) {
     app.use('/news', newsRouter);
+    app.use('/courses', coursesRouter);
 
     app.use('/', siteRouter);
 }
