94b0f21c09e3cbdf3bc2c01bc29a185187bb412a
trissdafo2411 <xaxixo333@gmail.com>
2021-06-13 00:28:37 +0700

    Delete courses

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -9,6 +9,7 @@ router.get('/create', courseController.create);
 router.post('/store', courseController.store);
 router.get('/:slug', courseController.show);
 router.put('/:id', courseController.update);
+router.delete('/:id', courseController.destroy);
 router.get('/:id/edit', courseController.edit);
 
 module.exports = router;
