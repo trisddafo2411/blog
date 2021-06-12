de2de1cee08e48dfbf514be5b7962b1ea13d80fe
trissdafo2411 <xaxixo333@gmail.com>
2021-06-12 18:20:59 +0700

    edit/update courses

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -8,5 +8,7 @@ const courseController = require('../app/controllers/CourseController');
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
 router.get('/:slug', courseController.show);
+router.put('/:id', courseController.update);
+router.get('/:id/edit', courseController.edit);
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const newsRouter = require('./news');
+const meRouter = require('./me');
 const siteRouter = require('./site');
 const coursesRouter = require('./courses');
 function route(app) {
     app.use('/news', newsRouter);
+    app.use('/me', meRouter);
     app.use('/courses', coursesRouter);
 
     app.use('/', siteRouter);
