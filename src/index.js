06a278751b3fdf5a51e9697161e7baea6020b32c
trissdafo2411 <xaxixo333@gmail.com>
2021-06-20 02:25:26 +0700

    sort courses

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
+
 const route = require('./routes');
 const db = require('./config/db');
 
@@ -23,6 +25,9 @@ app.use(express.json());
 
 app.use(methodOverride('_method'));
 
+//Custom middlewares
+app.use(SortMiddleware);
+
 //HTTP logger
 app.use(morgan('combined'));
 
@@ -33,6 +38,28 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending',
+                };
+
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+
+                const icon = icons[sortType];
+                const type = types[sortType];
+
+                return `<a href="?_sort&column=${field}&type=${type}">
+                <span class="${icon}"></span>
+              </a>`;
+            },
         },
     }),
 );
